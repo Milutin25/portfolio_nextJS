3ff2c680df99cd148b437a35b773194756fc8179
Natasa Stefanovic <natasa@Natasas-MacBook-Air.local>
2024-09-26 12:24:12 -0500

    tags added to Schema and displayed

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { defineQuery } from "next-sanity";
 import imageUrlBuilder from '@sanity/image-url';
-
-
 import { client } from "../sanity/client";
 import Cursor from "@/app/components/cursor";
 import NavBar from "../components/navBar";
@@ -15,53 +13,84 @@ const builder = imageUrlBuilder(client);
 const BLOG_QUERY = defineQuery(`*[
   _type == "blog"
   && defined(slug.current)
-]{_id, name, image, slug, date}|order(date desc)`);
+]{_id, name, image, slug, date, tags}|order(date desc)`);
 
 export default async function IndexPage() {
-  const blogs = await client.fetch(BLOG_QUERY, {}, options);
+  const blogs: Blog[] = await client.fetch(BLOG_QUERY, {}, options);
+
+  interface Blog {
+    _id: number; 
+    image: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+    };
+    slug: {
+      current: string;
+    };
+    name: string;
+    date: string;
+    tags: string[];
+  }
+
+  const allTags = blogs.map(blog => blog.tags);
+  const uniqueTags = Array.from(new Set(allTags));
 
   return (
     <>
-    <Cursor/>
-    <div className="mouse-cursor">
-          <div className="cursor-inner"></div>
-          <div className="cursor-outer"></div>
+      <Cursor />
+      <div className="mouse-cursor">
+        <div className="cursor-inner"></div>
+        <div className="cursor-outer"></div>
       </div>
-    <NavBar/>
-    <HambMenu/>
-    <div className="kioto_tm_section">
-      <div className="container">
-      <div className="kioto_tm_title pt-20 pb-10 ">
-        <span>// Blogs</span>
+      <NavBar />
+      <HambMenu />
+      <div className="kioto_tm_section">
+        <div className="container">
+          <div className="kioto_tm_title pt-20 pb-10 ">
+            <span>// Blogs</span>
+          </div>
+          <Link href="">
+          <div className="tags">
+            {uniqueTags.length > 0 && (
+              <div className="tag-list">
+                {uniqueTags.map((tag, index) => (
+                  <span key={index} className="tag">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            )}
+          </div>
+          </Link>
+          <div className="blogs">
+            <ul>
+              {blogs.map((blog: Blog) => (
+                <li key={blog._id}>
+                  <Link href={`/blogs/${blog.slug.current}`}>
+                    {blog.image && (
+                      <Image
+                        className='blogimg'
+                        src={builder.image(blog.image).url()}
+                        alt={blog.name}
+                        height={200}
+                        width={300}
+                      />
+                    )}
+                    <h2>{blog.name}</h2>
+                    {blog.date && (
+                      <p>
+                        {new Date(blog.date).toLocaleDateString()}
+                      </p>
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="blogs">
-      <ul>
-        {blogs.map((blog: any) => (
-          <li key={blog._id}>
-            <Link
-              href={`/blogs/${blog?.slug?.current}`}
-            >
-              {blog?.image && (
-                 <Image className='blogimg' 
-                    src={builder.image(blog.image).url()} 
-                    alt={blog.name} 
-                    height={200}
-                    width={300}
-                    />
-              )}
-              <h2>{blog?.name}</h2>
-              {blog?.date && (
-                <p>
-                  {new Date(blog.date).toLocaleDateString()}
-                </p>
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      </div>
-      </div>
-    </div>
     </>
   );
 }
