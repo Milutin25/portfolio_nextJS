c71882efedca8cb0b569b48e4acf54eaaa2c8b42
Natasa Stefanovic <natasa@Natasas-MacBook-Air.local>
2024-09-30 15:30:42 -0500

    added .env.local

--- app/config/supabaseClient.tsx
+++ app/config/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ucjgvndhcpnpuadmrupg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjamd2bmRoY3BucHVhZG1ydXBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2NjYwNzAsImV4cCI6MjAzOTI0MjA3MH0.D3U03MjS2shdZTxL0O3LXWKtLbVVTR4s3PwryaGc6BI";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 let supabase: any | null = null;
 
